3820afe9ca58a83cf56543671ff0e6a8de5b69f8
exbb2 <kudahkukarek@gmail.com>
2012-09-18 14:34:47 +0300

    .

--- beta-pack.sh
+++ beta-pack.sh
@@ -6,6 +6,5 @@ echo "Packaging DOS"
 wine sh build-production.sh &&\
  rm -f BlastItWithPiss-BETA-0.0.0.0-windows-x86.7z ;\
  7za a BlastItWithPiss-BETA-0.0.0.0-windows-x86.7z dos-dist/./BlastItWithPiss
-git commit -a
-git push
+git commit -a && git push
 echo "Done packaging, now upload those https://github.com/exbb2/BlastItWithPiss/downloads"
