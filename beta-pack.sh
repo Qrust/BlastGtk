08dd21cad1774ff6ede3b44b1d6cee40f53b19b5
exbb2 <kudahkukarek@gmail.com>
2012-09-18 20:23:58 +0300

    ю

--- beta-pack.sh
+++ beta-pack.sh
@@ -9,6 +9,6 @@ wine sh build-production.sh &&\
  rm -f BlastItWithPiss-BETA-windows-x86-0.0.0.0.zip ;\
  zip -r BlastItWithPiss-BETA-windows-x86-0.0.0.0.zip dos-dist/./BlastItWithPiss)&
 fg
-fg
+fg # wait until all builds finish
 git commit -a && git push
 echo "Done packaging, now upload those https://github.com/exbb2/BlastItWithPiss/downloads"
