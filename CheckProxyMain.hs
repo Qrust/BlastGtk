8bc93cdfd99b24046ac97acab575e919cafc6030
exbb2 <kudahkukarek@gmail.com>
2012-10-23 20:52:38 +0300

    prxchfx

--- CheckProxyMain.hs
+++ CheckProxyMain.hs
@@ -121,11 +121,9 @@ mainloop board Config{..} ips mvs = do
                 httpSetProxy (readBlastProxy socks ip)
             unless quiet $ liftIO $ putStrLn $ ip ++ ": Поcтим"
             liftIO . putMVar m . fst =<<
-                post =<<
-                    prepare board (Just thread)
-                        (PostData "САЖА" txt Nothing True False False False) "03AHJ_VutW6y0VOt928pITpHtSRO6mM4Vk-iou_VVxKkC5MCxKKU-rSCdQT-yqaGxHg0y-YZNKDD_n_-bUNFSVDB-G_db5J4RbLIvI-ysf8fd2dXj4Xt6bwG0CRLRgmDrc-NmKQBn89GXVTpEZa2iTJF3Hny3F8e5aNw" "reading speed"
-                        (ssachBoard board ++ "wakaba.pl")
-                        [field "akane" ""] ssachLengthLimit
+                post =<< prepare board (Just thread)
+                    (PostData "САЖА" txt Nothing True False False False) "03AHJ_VutW6y0VOt928pITpHtSRO6mM4Vk-iou_VVxKkC5MCxKKU-rSCdQT-yqaGxHg0y-YZNKDD_n_-bUNFSVDB-G_db5J4RbLIvI-ysf8fd2dXj4Xt6bwG0CRLRgmDrc-NmKQBn89GXVTpEZa2iTJF3Hny3F8e5aNw" "reading speed"
+                    (ssachBoard board ++ "wakaba.pl") ssachLastRecordedWakabaplAndFields ssachLengthLimit
         return (ip, m)
     unless quiet $ putStrLn $ "Передышка: " ++ show timeout ++ " секунд..., Ещё не запущено: " ++ show (length ni) ++ " проксей."
     threadDelay $ timeout * 1000000
